02ea1d9aef4cf69989e1e3410808cdf7b0058989
Marti D <me@martidolce.com>
2021-04-18 20:31:32 -0400

    test passing
    Signed-off-by: Marti D <me@martidolce.com>

--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,10 @@ ActiveRecord::Schema.define(version: 20210418215032) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "category_id"
+    t.string   "published_status"
   end
 
 end
